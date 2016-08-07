331740c006a9d79129ddf56cf59e1f58939b896b
roto <roto@MacBook-Air.local>
2016-08-07 12:15:23 +0900

    [add]ドロップダウンの追加 onenuiを用いたボタンを追加

--- hj/www/js/app.js
+++ hj/www/js/app.js
@@ -1,122 +1,168 @@
 
 var app = {
-    initialize: function() {
-        this.bindEvents();
-        this.showMainPage();
-    },
-    bindEvents: function() {
+  initialize: function() {
+    this.bindEvents();
+    this.showMainPage();
+  },
+  bindEvents: function() {
+    var TOUCH_START = 'touchstart';
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+    refreshButton.addEventListener(TOUCH_START, this.refreshDeviceList, false);
+    sendButton.addEventListener(TOUCH_START, this.sendData, false);
+    disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
+    deviceList.addEventListener(TOUCH_START, this.connect, false);
+    deviceButton.addEventListener(TOUCH_START,this.search,false);
 
-        var TOUCH_START = 'touchstart';
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        refreshButton.addEventListener(TOUCH_START, this.refreshDeviceList, false);
-        sendButton.addEventListener(TOUCH_START, this.sendData, false);
-        disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
-        deviceList.addEventListener('touchstart', this.connect, false);
-    },
-    onDeviceReady: function() {
-        app.refreshDeviceList();
-    },
-    refreshDeviceList: function() {
-        bluetoothSerial.list(app.onDeviceList, app.onError);
-        console.log("update")
-    },
-    onDeviceList: function(devices) {
-        var option;
+  },
+  onDeviceReady: function() {
+    app.refreshDeviceList();
+  },
+  refreshDeviceList: function() {
+    bluetoothSerial.list(app.onDeviceList, app.onError);
+    console.log("update");
 
-        // remove existing devices
-        deviceList.innerHTML = "";
-        app.setStatus("");
+  },
+  onDeviceList: function(devices) {
+    var option;
+    // remove existing devices
+    deviceList.innerHTML = "";
+    app.setStatus("");
 
-        devices.forEach(function(device) {
+    devices.forEach(function(device) {
 
-            var listItem = document.createElement('li'),
-                html = '<b>' + device.name + '</b><br/>' + device.id;
+      var listItem = document.createElement('li'),
+      html = '<b>' + device.name + '</b><br/>' + device.id;
 
-            listItem.innerHTML = html;
+      listItem.innerHTML = html;
 
-              listItem.dataset.deviceId = device.id;
-            
-            deviceList.appendChild(listItem);
-        });
+      listItem.dataset.deviceId = device.id;
 
-        if (devices.length === 0) {
+      deviceList.appendChild(listItem);
+    });
 
-            option = document.createElement('option');
-            option.innerHTML = "No Bluetooth Devices";
-            deviceList.appendChild(option);
+    if (devices.length === 0) {
 
-            if (cordova.platformId === "ios") { // BLE
-                app.setStatus("No Bluetooth Peripherals Discovered.");
-            } else { // Android or Windows Phone
-                app.setStatus("Please Pair a Bluetooth Device.");
-            }
+      option = document.createElement('option');
+      option.innerHTML = "No Bluetooth Devices";
+      deviceList.appendChild(option);
 
-        } else {
-            app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
-        }
+      if (cordova.platformId === "ios") { // BLE
+        app.setStatus("No Bluetooth Peripherals Discovered.");
+      } else { // Android or Windows Phone
+        app.setStatus("Please Pair a Bluetooth Device.");
+      }
 
-    },
-    connect: function(e) {
-        var onConnect = function() {
-                // subscribe for incoming data
-                bluetoothSerial.subscribe('\n', app.onData, app.onError);
-
-                resultDiv.innerHTML = "";
-                app.setStatus("Connected");
-                app.showDetailPage();
-            };
-
-        var deviceId = e.target.dataset.deviceId;
-        if (!deviceId) { // try the parent
-            deviceId = e.target.parentNode.dataset.deviceId;
-        }
-
-        bluetoothSerial.connect(deviceId, onConnect, app.onError);
-    },
-    onData: function(data) { // data received from Arduino
-        console.log(data);
-        resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + data + "<br/>";
-        resultDiv.scrollTop = resultDiv.scrollHeight;
-    },
-    sendData: function(event) { // send data to Arduino
-
-        var success = function() {
-            console.log("success");
-            resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + messageInput.value + "<br/>";
-            resultDiv.scrollTop = resultDiv.scrollHeight;
-        };
-
-        var failure = function() {
-            alert("Failed writing data to Bluetooth peripheral");
-        };
-
-        var data = messageInput.value;
-        bluetoothSerial.write(data, success, failure);
-    },
-    disconnect: function(event) {
-        bluetoothSerial.disconnect(app.showMainPage, app.onError);
-    },
-    showMainPage: function() {
-        mainPage.style.display = "";
-        detailPage.style.display = "none";
-    },
-    showDetailPage: function() {
-        mainPage.style.display = "none";
-        detailPage.style.display = "";
-    },
-    setStatus: function(message) {
-        console.log(message);
-
-        window.clearTimeout(app.statusTimeout);
-        statusDiv.innerHTML = message;
-        statusDiv.className = 'fadein';
-
-        // automatically clear the status with a timer
-        app.statusTimeout = setTimeout(function () {
-            statusDiv.className = 'fadeout';
-        }, 5000);
-    },
-    onError: function(reason) {
-        alert("ERROR: " + reason); // real apps should use notification.alert
+    } else {
+      app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
     }
+
+  },
+  connect: function(e) {
+    var onConnect = function() {
+      // subscribe for incoming data
+      bluetoothSerial.subscribe('\n', app.onData, app.onError);
+
+      resultDiv.innerHTML = "";
+      app.setStatus("Connected");
+      app.showDetailPage();
+    };
+
+    var deviceId = e.target.dataset.deviceId;
+    if (!deviceId) { // try the parent
+      deviceId = e.target.parentNode.dataset.deviceId;
+    }
+
+    bluetoothSerial.connect(deviceId, onConnect, app.onError);
+  },
+  onData: function(data) { // data received from Arduino
+    console.log(data);
+    resultDiv.innerHTML = resultDiv.innerHTML + "Received: " + data + "<br/>";
+    resultDiv.scrollTop = resultDiv.scrollHeight;
+  },
+  sendData: function(event) { // send data to Arduino
+
+    var success = function() {
+      console.log("success");
+      resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + messageInput.value + "<br/>";
+      resultDiv.scrollTop = resultDiv.scrollHeight;
+    };
+
+    var failure = function() {
+      alert("Failed writing data to Bluetooth peripheral");
+    };
+
+    var data = messageInput.value;
+    bluetoothSerial.write(data, success, failure);
+  },
+  disconnect: function(event) {
+    bluetoothSerial.disconnect(app.showMainPage, app.onError);
+  },
+  showMainPage: function() {
+    mainPage.style.display = "";
+    detailPage.style.display = "none";
+  },
+  showDetailPage: function() {
+    mainPage.style.display = "none";
+    detailPage.style.display = "";
+  },
+  setStatus: function(message) {
+    console.log(message);
+
+    window.clearTimeout(app.statusTimeout);
+    statusDiv.innerHTML = message;
+    statusDiv.className = 'fadein';
+
+    // automatically clear the status with a timer
+    app.statusTimeout = setTimeout(function () {
+      statusDiv.className = 'fadeout';
+    }, 5000);
+  },
+  onError: function(reason) {
+    alert("ERROR: " + reason); // real apps should use notification.alert
+  },
+    
+    /*ドロップダウンに検索したデバイスを追加する*/
+  search: function(){
+       bluetoothSerial.list(app.searchDevice, app.onError);
+   
+  },
+    searchDevice: function(devices){
+     var select=document.getElementById('sample');
+        devices.forEach(function(device){
+            var name=device.name;
+            var id=device.id;
+     var opt = document.createElement('option');
+	var text = document.createTextNode(name);
+	opt.appendChild(text);
+
+	// option要素を追加
+	select.appendChild(opt);
+            
+        });
+  
+},
+    /*Bluetoothで受信した文字列のデータを整数型に分割する*/
+    receiveData: function(){
+        bluetoothSerial.subscribe("\n",app.splitString,app.onError);
+    },
+    splitString: function(data){
+        var acc=[]
+        var gyr=[];
+        var strings=data.split(",");
+        for(var i=0;i<strings.length/2;i++){
+            acc[i]=Number(strings[i]);
+            gyr[i+3]=Number(string[i+3]);
+        }
+    } 
 };
+/*onsenuiでドロップダウンを実現するために使う？まだ実装できてない*/
+var module = ons.bootstrap();
+module.controller('AppController', function($scope) {
+  ons.createPopover('popover.html').then(function(popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.show = function(e) {
+    $scope.popover.show(e);
+  };
+});
