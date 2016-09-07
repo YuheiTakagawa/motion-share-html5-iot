422c42fcec0194c84d64346d36ee0a6fbd8acf79
Koki Watanabe <gateway0616@gmail.com>
2016-09-07 18:02:45 +0900

    [add]握手判断（まだ判断があまい）

--- motionShare/www/js/handshake.js
+++ motionShare/www/js/handshake.js
@@ -1,30 +1,60 @@
-$(function(){
+(function () {
 
-  // DeviceMotion Event
-  window.addEventListener("devicemotion", devicemotionHandler);
+  var cnt =0;
+  var flag =5;
+
+
+  $(function () {
+    // DeviceMotion Event
+    window.addEventListener("devicemotion", devicemotionHandler);
+  });
+
 
   // 加速度が変化
   function devicemotionHandler(event) {
+
+    // 加速度
+    // X軸
     var x = event.acceleration.x;
+    // Y軸
     var y = event.acceleration.y;
+    // Z軸
     var z = event.acceleration.z;
 
-/*
-    $("accelerationX").text(x);
-    $("accelerationY").text(y);
-    $("accelerationZ").text(z);
-    */
+
     document.getElementById('accelerationX').innerHTML = x;
     document.getElementById('accelerationY').innerHTML = y;
     document.getElementById('accelerationZ').innerHTML = z;
+
+
+    document.getElementById('count').innerHTML = cnt;
+
+    var l = 7;
+
+    if (x > l) { // 右
+      cnt ++;
+    }
+
+    else if (x < -l) { // 左
+      cnt ++;
+    }
+
+    if(cnt > flag){
+      alert('握手');
+      cnt = 0;
+    }
   }
-});
+})();
+
+
+
+
+
+
+
+
+
 /*
-
-
-
-
-
 
 
 (function () {
