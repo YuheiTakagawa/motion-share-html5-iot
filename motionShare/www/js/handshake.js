b37751039c2fdafa197b5a14d97beb07707d431a
Koki Watanabe <gateway0616@gmail.com>
2016-09-08 15:55:26 +0900

    [add]角速度 3軸変化値取得・表示

--- motionShare/www/js/handshake.js
+++ motionShare/www/js/handshake.js
@@ -7,6 +7,7 @@
   $(function () {
     // DeviceMotion Event
     window.addEventListener("devicemotion", devicemotionHandler);
+    window.addEventListener("deviceorientation", deviceorientationHandler);
   });
 
 
@@ -38,4 +39,19 @@
       cnt = 0;
     }
   }
+
+  //角速度変化
+   function deviceorientationHandler(event) {
+    // X軸
+    var beta = event.beta;
+    // Y軸
+    var gamma = event.gamma;
+    // Z軸
+    var alpha = event.alpha;
+
+    document.getElementById('beta').innerHTML = beta;
+    document.getElementById('gamma').innerHTML = gamma;
+    document.getElementById('alpha').innerHTML = alpha;
+   }
+
 })();
