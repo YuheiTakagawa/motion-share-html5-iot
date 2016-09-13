4b8b3fa7017f86d4e2d6f5d3bd35ea83f847411a
yuhei <b1014223@fun.ac.jp>
2016-09-13 18:25:48 +0900

    [add]smartButtonのイベントリスナの設定と関数chooseSmartを作成

--- motionShare/www/js/_page1.js
+++ motionShare/www/js/_page1.js
@@ -11,6 +11,7 @@ var bluetoothFanc = {
     document.addEventListener('deviceready', this.onDeviceReady, false);
     deviceButton.addEventListener(TOUCH_START,this.searchDevice,false);
     DeviceSelect.addEventListener(TOUCH_START, this.connect, false);
+    smartButton.addEventListener(TOUCH_START,this.chooseSmart,false);
     disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
   },
 
@@ -19,6 +20,7 @@ var bluetoothFanc = {
 
     //this.autoConnect();
   },
+
 
   //起動時に自動ペアリングする
   autoConnect: function(){
@@ -32,6 +34,7 @@ var bluetoothFanc = {
     }
   },
 
+
   //デバイスに接続した時の処理
   connect: function(e) {
     //接続したデバイスの情報を取得
@@ -43,20 +46,25 @@ var bluetoothFanc = {
       alert("success:"+deviceId);}, bluetoothFanc.onError);
     },
 
+
+    //bluetooth端末との接続を解除する
     disconnect: function(event) {
       bluetoothSerial.disconnect(
         function(){alert("ペアリング解除成功")}, bluetoothFanc.onError);
       },
 
+
+      //デバイスを検索する
       searchDevice: function(){
-        //showPages.showSelectDevicePage();
-        //デバイスを検索する
         bluetoothSerial.list(updateTag.searchResult, bluetoothFanc.onError);
       },
 
+
+      //データを受信する
       receiveData: function(){
         bluetoothSerial.subscribe("\n",bluetoothFanc.splitString,bluetoothFanc.onError);
       },
+
 
       //受信した文字列を整数に変換する
       splitString: function(data){
@@ -68,6 +76,7 @@ var bluetoothFanc = {
           gyr[i+3]=Number(string[i+3]);  //gyrX,gryY,gryZ
         }
       },
+
 
       onError: function(reason) {
         alert("ERROR: " + reason);
@@ -102,10 +111,17 @@ var bluetoothFanc = {
         deviceButton.innerHTML="<p>専用デバイス<br>"+name+"</p>";
       },
     };
-/*
-var bluetoothBasicFanc = {
-  //初期化
-  initialize: function() {
+
+
+
+
+
+
+
+    /*
+    var bluetoothBasicFanc = {
+    //初期化
+    initialize: function() {
     //alert("a");
     this.bindEvents();
     showPages.showMainPage();
@@ -113,73 +129,73 @@ var bluetoothBasicFanc = {
 
   //イベントの管理
   bindEvents: function() {
-    var TOUCH_START = 'touchstart';
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-    deviceButton.addEventListener(TOUCH_START,this.searchDevice,false);
-    deviceList.addEventListener(TOUCH_START, this.connect, false);
-    disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
-  },
+  var TOUCH_START = 'touchstart';
+  document.addEventListener('deviceready', this.onDeviceReady, false);
+  deviceButton.addEventListener(TOUCH_START,this.searchDevice,false);
+  deviceList.addEventListener(TOUCH_START, this.connect, false);
+  disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
+},
 
-  onDeviceReady: function() {
-    //cordovaがOnになった時に行いたい処理を記述
+onDeviceReady: function() {
+//cordovaがOnになった時に行いたい処理を記述
 
-    //this.autoConnect();
-  },
+//this.autoConnect();
+},
 
-  //起動時に自動ペアリングする
-  autoConnect: function(){
-    var deviceId=getDeviceInfo.getDeviceId();
-    if(deviceId!=""){
-      bluetoothSerial.connect(deviceId,function(){
-        alert("success:"+localStorage.name);
-        //this.receiveData();
-      },this.onError);
-      updateTag.changeButtonName();
-    }
-  },
+//起動時に自動ペアリングする
+autoConnect: function(){
+var deviceId=getDeviceInfo.getDeviceId();
+if(deviceId!=""){
+bluetoothSerial.connect(deviceId,function(){
+alert("success:"+localStorage.name);
+//this.receiveData();
+},this.onError);
+updateTag.changeButtonName();
+}
+},
 
-  //デバイスに接続した時の処理
-  connect: function(e) {
-    //接続したデバイスの情報を取得
-    var deviceName = e.target.dataset.deviceName;
-    var deviceId = e.target.dataset.deviceId;
+//デバイスに接続した時の処理
+connect: function(e) {
+//接続したデバイスの情報を取得
+var deviceName = e.target.dataset.deviceName;
+var deviceId = e.target.dataset.deviceId;
 
-    updateTag.changeButtonName(deviceName);
-    //deviceBeanは１スプリント目では使用しない
-    ////setDeviceInfo.setDeviceName(deviceName);
-    ////setDeviceInfo.setDeviceId(deviceId);
-    bluetoothSerial.connect(deviceId, function(){
-      alert("success:"+deviceId);}, bluetoothBasicFanc.onError);
-    },
+updateTag.changeButtonName(deviceName);
+//deviceBeanは１スプリント目では使用しない
+////setDeviceInfo.setDeviceName(deviceName);
+////setDeviceInfo.setDeviceId(deviceId);
+bluetoothSerial.connect(deviceId, function(){
+alert("success:"+deviceId);}, bluetoothBasicFanc.onError);
+},
 
-    disconnect: function(event) {
-      bluetoothSerial.disconnect(
-        showPages.showMainPage, bluetoothBasicFanc.onError);
-      },
+disconnect: function(event) {
+bluetoothSerial.disconnect(
+showPages.showMainPage, bluetoothBasicFanc.onError);
+},
 
-      searchDevice: function(){
-        showPages.showSelectDevicePage();
-        //デバイスを検索する
-        bluetoothSerial.list(updateTag.searchResult, bluetoothBasicFanc.onError);
-      },
+searchDevice: function(){
+showPages.showSelectDevicePage();
+//デバイスを検索する
+bluetoothSerial.list(updateTag.searchResult, bluetoothBasicFanc.onError);
+},
 
-      receiveData: function(){
-        bluetoothSerial.subscribe("\n",bluetoothBasicFanc.splitString,bluetoothBasicFanc.onError);
-      },
+receiveData: function(){
+bluetoothSerial.subscribe("\n",bluetoothBasicFanc.splitString,bluetoothBasicFanc.onError);
+},
 
-      //受信した文字列を整数に変換する
-      splitString: function(data){
-        var acc=[];
-        var gyr=[];
-        var strings=data.split(",");
-        for(var i=0;i<strings.length/2;i++){
-          acc[i]=Number(strings[i]);    //accX,accY,accZ
-          gyr[i+3]=Number(string[i+3]);  //gyrX,gryY,gryZ
-        }
-      },
+//受信した文字列を整数に変換する
+splitString: function(data){
+var acc=[];
+var gyr=[];
+var strings=data.split(",");
+for(var i=0;i<strings.length/2;i++){
+acc[i]=Number(strings[i]);    //accX,accY,accZ
+gyr[i+3]=Number(string[i+3]);  //gyrX,gryY,gryZ
+}
+},
 
-      onError: function(reason) {
-        alert("ERROR: " + reason);
-      }
-    };
+onError: function(reason) {
+alert("ERROR: " + reason);
+}
+};
 */
