5defc88184a4a7f019ebd6057a736b67ebaeb587
Koki Watanabe <gateway0616@gmail.com>
2016-09-13 13:12:10 +0900

    [add]読み込んではいないが、ボタン色チェンジャー機能の追加

--- motionShare/www/js/page1.js
+++ motionShare/www/js/page1.js
@@ -1,81 +1,35 @@
 
+//デフォルト設定では本体がアクティブ状態。今後の開発で専用デバイスが接続されていたら自動で切り替えできるようにしたい
+tmpBtnTrigger(1);
 
-var bluetoothBasicFanc = {
-  //初期化
-  initialize: function() {
-    //alert("a");
-    this.bindEvents();
-    showPages.showMainPage();
-  },
+$("#DeviceSelect li").on("click", deviceButton);
+$("#smartButton").on("click", smartButton);
 
-  //イベントの管理
-  bindEvents: function() {
-    var TOUCH_START = 'touchstart';
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-    deviceButton.addEventListener(TOUCH_START,this.searchDevice,false);
-    deviceList.addEventListener(TOUCH_START, this.connect, false);
-    disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
-  },
 
-  onDeviceReady: function() {
-    //cordovaがOnになった時に行いたい処理を記述
 
-    //this.autoConnect();
-  },
+function tmpBtnTrigger(num){
+  switch (num) {
+    case 0://「専用デバイス」選択状態
+    $("#deviceButton").addClass("cyan");
+    $("#smartButton").addClass("disabled");
+    break;
+    case 1://「本体」選択状態
+    $("#smartButton").addClass("cyan");
+    $("#deviceButton").addClass("disabled");
+    break;
+  }
+}
 
-  //起動時に自動ペアリングする
-  autoConnect: function(){
-    var deviceId=getDeviceInfo.getDeviceId();
-    if(deviceId!=""){
-      bluetoothSerial.connect(deviceId,function(){
-        alert("success:"+localStorage.name);
-        //this.receiveData();
-      },this.onError);
-      updateTag.changeButtonName();
-    }
-  },
 
-  //デバイスに接続した時の処理
-  connect: function(e) {
-    //接続したデバイスの情報を取得
-    var deviceName = e.target.dataset.deviceName;
-    var deviceId = e.target.dataset.deviceId;
+//専用デバイスボタンのトリガー処理
+function deviceButton(){
+  $("#deviceButton").addClass("cyan");
+  $("#deviceButton").removeClass("disabled");
+  $("#smartButton").addClass("disabled");
+}
 
-    updateTag.changeButtonName(deviceName);
-    //deviceBeanは１スプリント目では使用しない
-    ////setDeviceInfo.setDeviceName(deviceName);
-    ////setDeviceInfo.setDeviceId(deviceId);
-    bluetoothSerial.connect(deviceId, function(){
-      alert("success:"+deviceId);}, bluetoothBasicFanc.onError);
-    },
-
-    disconnect: function(event) {
-      bluetoothSerial.disconnect(
-        showPages.showMainPage, bluetoothBasicFanc.onError);
-      },
-
-      searchDevice: function(){
-        showPages.showSelectDevicePage();
-        //デバイスを検索する
-        bluetoothSerial.list(updateTag.searchResult, bluetoothBasicFanc.onError);
-      },
-
-      receiveData: function(){
-        bluetoothSerial.subscribe("\n",bluetoothBasicFanc.splitString,bluetoothBasicFanc.onError);
-      },
-
-      //受信した文字列を整数に変換する
-      splitString: function(data){
-        var acc=[];
-        var gyr=[];
-        var strings=data.split(",");
-        for(var i=0;i<strings.length/2;i++){
-          acc[i]=Number(strings[i]);    //accX,accY,accZ
-          gyr[i+3]=Number(string[i+3]);  //gyrX,gryY,gryZ
-        }
-      },
-
-      onError: function(reason) {
-        alert("ERROR: " + reason);
-      }
-    };
+//本体ボタンのトリガー処理
+function smartButton(){
+  $("#smartButton").removeClass("disabled");
+  $("#deviceButton").addClass("disabled");
+}
