1ad00e43095ef8f24f574d87c2e19ae0d7844a45
Koki Watanabe <gateway0616@gmail.com>
2016-09-30 18:35:12 +0900

    [add] index で読み込み

---
+++ motionshere/platforms/android/assets/www/js/connectServer.js
@@ -0,0 +1,7 @@
+//recodemo.herokuapp.com
+var url = "https://motion-share.herokuapp.com"; //websocketサーバのURL。
+var socket = io.connect(url);
+socket.on("connect", function() {
+  alert("connected");
+  socket.emit("html5_test", 0);
+});
