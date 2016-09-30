9a114f20dfdca31f5c2468b690e5829bff22dab9
Koki Watanabe <gateway0616@gmail.com>
2016-09-30 19:24:39 +0900

    [modify]接続完了のalert表示

--- motionshere/www/js/connectServer.js
+++ motionshere/www/js/connectServer.js
@@ -2,6 +2,6 @@
 var url = "https://motion-share.herokuapp.com"; //websocketサーバのURL。
 var socket = io.connect(url);
 socket.on("connect", function() {
-  alert("connected");
-  socket.emit("html5_test", 0);
+  alert("ms serverに接続完了");
+  //socket.emit("html5_test", 0);
 });
