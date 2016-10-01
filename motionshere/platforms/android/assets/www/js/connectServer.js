448b3854b93bd4d22469d69666dbe125b91c1553
Koki Watanabe <gateway0616@gmail.com>
2016-10-01 20:25:56 +0900

    [add] inputにセレクタでイベントを仕掛ける

--- motionshere/platforms/android/assets/www/js/connectServer.js
+++ motionshere/platforms/android/assets/www/js/connectServer.js
@@ -5,3 +5,11 @@ socket.on("connect", function() {
   alert("ms serverに接続完了");
   //socket.emit("html5_test", 0);
 });
+
+
+$(document).ready(function(){
+     $("#fileInput").change(function(event){
+         var file = event.target.files[0];
+         upload(file);//ファイルを送る関数
+      });
+});
