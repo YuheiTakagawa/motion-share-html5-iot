82c4cc64d084c8a68a8feb453a5e3a0f98193590
Koki Watanabe <gateway0616@gmail.com>
2016-10-01 19:11:50 +0900

    [add] pageControl.jsの追加

---
+++ motionshere/www/js/pageControl.js
@@ -0,0 +1,30 @@
+$(function() {
+  $("#view").load("home.html");
+});
+//パーツ読み込み
+function PageControll(PageNum){
+  $(function() {
+    switch(PageNum){
+      case 0:
+      $("#view").innerHTML = "<div id='home'></div>";
+      $("#view").load("home.html");
+      $(".brand-logo").html("MotionShare");
+      break;
+      case 1:
+      $("#view").innerHTML = "<div id='page1'></div>";
+      $("#view").load("page1.html");
+      $(".brand-logo").html("デバイス");
+      break;
+      case 2:
+      $("#view").innerHTML = "<div id='page2'></div>";
+      $("#view").load("page2.html");
+      $(".brand-logo").html("握手");
+      break;
+      case 3:
+      $("#view").innerHTML = "<div id='page3'></div>";
+      $("#view").load("page3.html");
+      $(".brand-logo").html("位置情報");
+      break;
+    }
+  });
+}
