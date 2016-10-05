621e4d0dc0b73cdef624ee31ce86bbdb9d1d708e
yuheiTakagawa <roto@roto-MacBook-Air.local>
2016-10-05 19:28:31 +0900

    [modify]マージミスを修正

--- motionshere/www/js/page1.js
+++ motionshere/www/js/page1.js
@@ -39,7 +39,9 @@ var bluetoothFanc = {
         bluetoothSerial.receiveData();
       },this.onError);
     }else{
-      alert("本体を設定しています。");
+      //alert("本体を設定しています。");
+      $(".info2").text('<br>sensor from this device ...OK');
+
     }
   },
 
