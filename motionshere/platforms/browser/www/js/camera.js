8ceebf777896b4e32f7dfc8a567771488edcc89e
Koki Watanabe <gateway0616@gmail.com>
2016-10-05 20:23:03 +0900

    [add] 一時的にコミット

--- motionshere/platforms/browser/www/js/camera.js
+++ motionshere/platforms/browser/www/js/camera.js
@@ -5,9 +5,8 @@ function getCameraBase64(){
                // cameraSuccess
                socket.emit("html5_test", imageData);
                alert("PHOTO GO TO SERVER");
-               $('#camera_pic')
-                   .css('display', 'block')
-                   .attr('src', 'data:image/jpeg;base64,' + imageData);
+
+               $('#camera_pic').attr('src', 'data:image/jpeg;base64,' + imageData);
           },
           function(message){
                // cameraError
@@ -17,9 +16,12 @@ function getCameraBase64(){
                quality : 75,
                destinationType : Camera.DestinationType.DATA_URL,
                sourceType : Camera.PictureSourceType.CAMERA, // 0:Photo Library, 1=Camera, 2=Saved Album
-               encodingType : 0, // 0=JPG 1=PNG
           });
 };
+
+function test(){
+  $('#camera_pic').attr('src', 'data:image/jpeg;base64,');
+}
 
 /*
 // カメラから画像を取得して、保存された画像のURIを取得する
