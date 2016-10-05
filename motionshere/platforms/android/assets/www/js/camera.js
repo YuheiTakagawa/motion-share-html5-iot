199e7547cc8ab19053ce22151359404113ca10bf
Koki Watanabe <gateway0616@gmail.com>
2016-10-05 13:15:37 +0900

    [add] bese64にエンコードした画像データをサーバに送信機能実装

--- motionshere/platforms/android/assets/www/js/camera.js
+++ motionshere/platforms/android/assets/www/js/camera.js
@@ -1,42 +1,70 @@
 // カメラから画像を取得して、Base64形式で取得する
 function getCameraBase64(){
-  navigator.camera.getPicture(
-    function(imageData){
-      // cameraSuccess
-      $('#camera_pic')
-      .css('display', 'block')
-      .attr('src', 'data:image/jpeg;base64,' + imageData);
-    },
-    function(message){
-      // cameraError
-      alert(message);
-    },
-    {
-      quality : 75,
-      destinationType : Camera.DestinationType.DATA_URL,
-      sourceType : Camera.PictureSourceType.CAMERA,
-    });
-  };
-
-  // カメラから画像を取得して、保存された画像のURIを取得する
-  function getCameraURI(){
-      navigator.camera.getPicture(
-          function(imageURI){
-              // cameraSuccess
-              $('#camera_pic')
+     navigator.camera.getPicture(
+          function(imageData){
+               // cameraSuccess
+               $('#camera_pic')
                    .css('display', 'block')
-                   .attr('src', imageURI);
+                   .attr('src', 'data:image/jpeg;base64,' + imageData);
           },
           function(message){
-              // cameraError
-              alert(message);
+               // cameraError
+               alert(message);
           },
           {
-              quality : 75,
-              destinationType : Camera.DestinationType.FILE_URI,
-              sourceType : Camera.PictureSourceType.CAMERA,
+               quality : 75,
+               destinationType : Camera.DestinationType.DATA_URL,
+               sourceType : Camera.PictureSourceType.CAMERA,
           });
-  };
+};
+
+/*
+// カメラから画像を取得して、保存された画像のURIを取得する
+function getCameraURI(){
+    navigator.camera.getPicture(
+        function(imageURI){
+            // cameraSuccess
+            $('#camera_pic')
+                 .css('display', 'block')
+                 .attr('src', imageURI);
+        },
+        function(message){
+            // cameraError
+            alert(message);
+        },
+        {
+            quality : 75,
+            destinationType : Camera.DestinationType.FILE_URI,
+            sourceType : Camera.PictureSourceType.CAMERA,
+        });
+};
+// ギャラリーなどから選択した画像のURIを取得する
+function getPhotoURI(){
+    navigator.camera.getPicture(
+        function(imageURI){
+            // cameraSuccess
+            $('#camera_pic')
+                 .css('display', 'block')
+                 .attr('src', imageURI);
+        },
+        function(message){
+            // cameraError
+            alert(message);
+        },
+        {
+            quality : 75,
+            destinationType : Camera.DestinationType.FILE_URI,
+            sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+        });
+};
+
+*/
+
+
+
+
+
+
 
   /*
   var pictureSource;   // picture source
