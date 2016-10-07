2fa7809b8cada0d8a90dec787e25c4f9e84defa0
Koki Watanabe <gateway0616@gmail.com>
2016-10-07 13:22:58 +0900

    [fix] bag fix socket関係のバグはまだ消しきれていない

--- motionshare/www/js/connectServer.js
+++ motionshare/www/js/connectServer.js
@@ -4,11 +4,3 @@ var socket = io.connect(url);
 socket.on("connect", function() {
   $(".info").text('connect server...OK');
 });
-
-
-function receiveData(){
-  alert("start receiveData");
-  socket.on('html5_test_show', function(rcvMsg) {
-    alert(rcvMsg);
-  });
-}
--- motionshare/www/js/modeChange.js
+++ motionshare/www/js/modeChange.js
@@ -1,48 +1,24 @@
-var changeMotionBool = false;
+var changeMotionBool = new Boolean(false);
+changeMotionBool = false; //true: 送信者 / false:受信者
 
-//モーションでデータ交換完了後，下の処理を実行する
-/*
-*/
+/*******  モード切り替え処理  *******/
 function modeChange(){
-  var checked = $("#modeChangeBtn").is(":checked");
+  changeMotionBool = !changeMotionBool;
 
-  if(checked == true || changeMotionBool == true) senderMode();
-  else if (checked == false || changeMotionBool == false) receiverMode();
+  if(changeMotionBool == true) senderMode();
+  else if(changeMotionBool == false) receiverMode();
 }
-
 
 /*******  送信者モードのときの処理  *******/
 function senderMode(){
   $("#modeStatus").html("送信者");
+  $('input[name="modeChangeBtn"]').prop("checked",true);
 }
-
 
 /*******  受信者モードのときの処理  *******/
 function receiverMode(){
   $("#modeStatus").html("");
+  $('input[name="modeChangeBtn"]').prop("checked",false);
   //サーバからデータ受信
   socket.on('html5_test_show', function(rcvMsg) { alert(rcvMsg); });
 }
-
-
-
-
-/*
-function modeChange(mode){
-if(mode==0){ //送信者モード
-mode = 1;
-$("#modeStatus").html("送信者");
-}else if(mode==1){ //受信者モード
-mode = 0;
-$("#modeStatus").html("");
-
-//alert("R-MODE OPEN");
-socket.on('html5_test_show', function(rcvMsg) {
-alert(rcvMsg);
-});
-
-
-}
-}
-
-*/
