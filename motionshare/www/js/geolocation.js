0ecd6a7710fd7199f8c73a0079f39add761ad569
Koki Watanabe <gateway0616@gmail.com>
2016-10-09 06:38:08 +0900

    [modify] gps取得処理のリファクタリングと時刻をgpsから取得する処理の実装

--- motionshare/www/js/geolocation.js
+++ motionshare/www/js/geolocation.js
@@ -1,84 +1,50 @@
 // 位置情報取得処理に渡すオプション
 var options = {
-  // 高精度な位置情報を取得するか(デフォルトはfalse)
   enableHightAccuracy: true,
-
-  // 取得した位置情報をキャッシュする時間(ミリ秒。デフォルトは0)
   maximumAge: 0,
-
-  // 何秒でタイムアウトとするか(ミリ秒。タイムアウトするとerrorCallback()がコールされる)
   timeout: 120000
 }
 
-// 位置情報取得処理が成功した時にコールされる関数
-// 引数として、coords(coordinates。緯度・経度など)とtimestamp(タイムスタンプ)の2つを持ったpositionが渡される
+var time;
+var geoData;
+
 function successCallback(position){
-  // メッセージを表示
-  document.getElementById("message").innerHTML += "API成功<br />";
+  var latitude         = position.coords.latitude; // 緯度(-180～180度)
+  var longitude        = position.coords.longitude; // 経度(-90～90度)
 
-  // 引数positionからcoordinates(緯度・経度など)を取り出す
-  // ただし、“★必ず取得できる”以外は中身が空っぽの可能性もある
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+  var date = now.getDate();
+  var h = now.getHours();
+  var m = now.getMinutes();
+  var s = now.getSeconds();
+  var nowTime = year + ',' +  month + ',' + date + ',' + h + ',' + m + ',' + s;
 
-  // 緯度(-180～180度) ★必ず取得できる
-  var latitude         = position.coords.latitude;
+  time = nowTime;
+  geoData;
 
-  // 経度(-90～90度) ★必ず取得できる
-  var longitude        = position.coords.longitude;
+  //alert(latitude + ',' + longitude + ',' + nowTime);
 
-  // 高度(m)
-  var altitude         = position.coords.altitude;
-
-  // 緯度・経度の誤差(m) ★必ず取得できる
-  var accuracy         = position.coords.accuracy;
-
-  // 高度の誤差(m)
-  var altitudeAccuracy = position.coords.altitudeAccuracy;
-
-  // 方角(0～360度)
-  var heading          = position.coords.heading;
-
-  // 速度(m/秒)
-  var speed            = position.coords.speed;
-
-
-  // 引数positionからtimestamp(位置情報を取得した時刻のミリ秒)を取り出す ★必ず取得できる
-  var timestamp = position.timestamp;
-
-  // timestampをDate型に変換
-  var successDate = new Date(timestamp);
-
-  // メッセージを表示
-  document.getElementById("message").innerHTML += "取得日時：" + successDate.toLocaleString() + "<br />";
-  document.getElementById("message").innerHTML += "緯度：" + latitude + " 度<br />";
-  document.getElementById("message").innerHTML += "経度：" + longitude + " 度<br />";
-  document.getElementById("message").innerHTML += "高度：" + altitude + " m<br />";
-  document.getElementById("message").innerHTML += "緯度・経度の誤差：" + accuracy + " m<br />";
-  document.getElementById("message").innerHTML += "高度の誤差：" + altitudeAccuracy + " m<br />";
-  document.getElementById("message").innerHTML += "方角：" + heading + " 度<br />";
-  document.getElementById("message").innerHTML += "速度：" + speed + " m/秒<br />";
+  //var altitude         = position.coords.altitude; // 高度(m)
+  //var accuracy         = position.coords.accuracy; // 緯度・経度の誤差(m)
+  //var altitudeAccuracy = position.coords.altitudeAccuracy; // 高度の誤差(m)
+  //var heading          = position.coords.heading; // 方角(0～360度)
+  //var speed            = position.coords.speed; // 速度(m/秒)
+  //var timestamp = position.timestamp; // 引数positionからtimestamp(位置情報を取得した時刻のミリ秒)
+  //var successDate = new Date(timestamp); // timestampをDate型に変換
 
   // 緯度・経度を地図上で確認するためにGoogleMapへのリンクを作成
+  /*
   document.getElementById("message").innerHTML += "<a target='_blank' href='https://maps.google.co.jp/maps?q="
   + latitude + "," + longitude + "+%28%E7%8F%BE%E5%9C%A8%E4%BD%8D%E7%BD%AE%29&iwloc=A'>緯度・経度をGoogleMapで確認</a><br /><br />";
+  */
 }
 
-// 位置情報取得処理が失敗した時にコールされる関数
-// 引数として、code(コード)とmessage(メッセージ)の2つを持ったpositionErrorが渡される
 function errorCallback(positionError){
-
-  // メッセージを表示
-  document.getElementById("message").innerHTML += "API失敗<br />";
-
-  // 引数positionErrorの中身2つを取り出す
-
-  // コード(1～3のいずれかの値)
   var code = positionError.code;
-
-  // メッセージ(開発者向けデバッグ用メッセージ)
   var message = positionError.message;
 
-
-  // コードに応じたメッセージを表示
   switch (code) {
     case positionError.PERMISSION_DENIED: // codeが1
     document.getElementById("message").innerHTML += "GeolocationAPIのアクセス許可がありません<br />";
@@ -90,50 +56,29 @@ function errorCallback(positionError){
     document.getElementById("message").innerHTML += "指定されたタイムアウト時間内に現在の位置情報を特定できませんでした<br />";
     break;
   }
-
-  // 開発者向けデバッグ用メッセージを表示
-  document.getElementById("message").innerHTML += message + "<br /><br />";
+  alert(message);
 }
 
 // watchPosition()の戻り値。この戻り値をclearWatch()に渡すことでwatch(監視)を停止させる。
 var watchId = null;
 
+
 // 位置情報監視開始
-function start(){
-  // watchIdが設定されているかをチェック
+function getGeoStart(){
   if (watchId == null) {
-    // 設定されていない → 監視中ではないのでwatch開始
-
-    // メッセージ表示領域をクリア
-    document.getElementById("message").innerHTML = "";
-
-    // ブラウザがGeolocation APIに対応しているかをチェック
     if (navigator.geolocation) {
-      // 対応している → 位置情報監視開始
-      // 取得成功時にsuccessCallback()、そして取得失敗時にerrorCallback()がコールされる
-      // optionsはgetCurrentPosition()に渡す設定値
       watchId = navigator.geolocation.watchPosition(successCallback, errorCallback , options);
-
-      // メッセージを表示。↑は非同期処理なので、直ぐにメッセージが表示される
-      document.getElementById("message").innerHTML = "API実行<br />";
     } else {
-      // 対応していない → alertを表示するのみ
       alert("Geolocation not supported in this browser");
     }
   }
 }
 
+
 // 位置情報監視停止
-function stop(){
-  // watchIdが設定されているかをチェック
+function getGeoStop(){
   if (watchId != null) {
-    // 設定されている → 監視中なのでwatch停止
     navigator.geolocation.clearWatch(watchId);
-
-    // watchIdを空にする
     watchId = null;
-
-    // メッセージを表示
-    document.getElementById("message").innerHTML += "clearWatch実行(watch停止)<br />";
   }
 }
