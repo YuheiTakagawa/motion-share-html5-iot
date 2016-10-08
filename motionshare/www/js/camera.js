5f4866e601d5689bc90a679bcddd5ea746c5977b
Koki Watanabe <gateway0616@gmail.com>
2016-10-08 15:17:26 +0900

    [add] フォトライブラリーで選択した画像をbase64にエンコードしてTOPに表示する機能の実装

--- motionshare/www/js/camera.js
+++ motionshare/www/js/camera.js
@@ -1,3 +1,5 @@
+var url;
+
 
 // カメラから画像を取得して、Base64形式で取得する
 function getCameraBase64(){
@@ -22,21 +24,24 @@ function getCameraBase64(){
   };
 
 
-    // ギャラリーなどから選択した画像のURIを取得する
-    function getPhotoURI(){
+  // ギャラリーなどから選択した画像のURIを取得する
+  function getPhotoURI(){
     navigator.camera.getPicture(
-        function(imageURI){
-            // cameraSuccess
-            $('#camera_pic').attr('src', imageURI);
-            $(".test").html(camera_pic.src);
-        },
-        function(message){
-            // cameraError
-            alert(message);
-        },
-        {
-            quality : 75,
-            destinationType : Camera.DestinationType.FILE_URI,
-            sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
-        });
-};
+      function(imageURI){
+        // cameraSuccess
+        $('#camera_pic')
+        .css('display', 'block')
+        .attr('src', 'data:image/jpeg;base64,' + imageURI);
+
+        $(".test").html(imageURI);
+      },
+      function(message){
+        // cameraError
+        alert(message);
+      },
+      {
+        quality : 75,
+        destinationType : Camera.DestinationType.DATA_URL,
+        sourceType : Camera.PictureSourceType.PHOTOLIBRARY,
+      });
+    };
