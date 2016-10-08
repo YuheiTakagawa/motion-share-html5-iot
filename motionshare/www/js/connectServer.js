383c01332e34b90c76394de30d39fa82dbeb641c
yuheiTakagawa <b1014223@fun.ac.jp>
2016-10-09 02:41:55 +0900

    [add]スケジュール・連絡先送信のコメント追加

--- motionshare/www/js/connectServer.js
+++ motionshare/www/js/connectServer.js
@@ -7,9 +7,11 @@ socket.on("connect", function() {
 
 //スケジュールを文字列に変換し，Base64でエンコードしてサーバに送信
 function sendSchedule(){
+  //localStorageにscheduleがあるときに処理を行う
   if(!(localStorage.schedule===void 0)){
     scheduleJson=JSON.parse(localStorage.schedule);
 
+    //直近のスケジュールを扱う
     sendingSche=scheduleJson["0"];
     sendingSche=JSON.stringify(sendingSche);
 
@@ -31,16 +33,23 @@ function sendSchedule(){
     }
   }
 
+  
+//連絡先のJSONを文字列に変換し，サーバに送信
   function sendContact(){
+    //localStorageにcontactがあるときに処理を行う
     if(!(localStorage.contact===void 0)){
+      //簡単に扱うために一時的にJSONを入れる変数
       var befferContact=JSON.parse(localStorage.contact);
+      //保存されたユーザ情報にはパスワードも含まれるため，パスワードを除いた4項目のJSONを再構築
       var sendingContact={
         "Name":befferContact.Name,
         "Id":befferContact.Id,
         "Phone":befferContact.Phone,
         "Mail":befferContact.Mail
       };
+      //名前が空文字であれば，不明とする
       if(sendingContact.Name=="") sendingContact.Name="不明";
+      //JSONを文字列にしてサーバに送信
       sendingContact=JSON.stringify(sendingContact);
       socket.emit("html5_test",sendingContact);
       alert("CONTACT GO TO SERVER");
