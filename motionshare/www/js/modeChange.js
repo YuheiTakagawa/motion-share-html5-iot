2e11f5a2add2824fecad28ab2d2f85b1791095c8
Koki Watanabe <gateway0616@gmail.com>
2016-10-09 21:12:45 +0900

    [add] 受信した画像データをhomeに表示する機能の実装

--- motionshare/www/js/modeChange.js
+++ motionshare/www/js/modeChange.js
@@ -9,6 +9,17 @@ function modeChange(){
   else if(changeMotionBool == false) receiverMode();
 }
 
+function modeFuncTrigger(){
+  if(changeMotionBool == true){
+    senderMode();
+    alert("Sender-Mode now");
+  }
+  else if(changeMotionBool == false){
+    receiverMode();
+    alert("Receiver-Mode now");
+  }
+}
+
 /*******  送信者モードのときの処理  *******/
 function senderMode(){
   $("#modeStatus").html("送信者");
@@ -22,6 +33,19 @@ function receiverMode(){
   //サーバからデータ受信
   socket.on('html5_test_show', function(rcvMsg) {
     alert("データを受信しました");
+
+    /*** 画像の送受信時の処理 (あとでconnectServer.js にてfunction作成) ****/
+    var data = localStorage.getItem('imageData');
+    data = rcvMsg;
+    if(data.length < 100){
+      $('.card-image').addClass('loadingWidth');
+      $('#camera_pic').attr('src', 'img/load.gif');
+    }else{
+      $('.card-image').removeClass('loadingWidth');
+      $('#camera_pic').attr('src', 'data:image/jpeg;charset=utf-8;base64,' + data);
+    }
+    /*****************************************************************/
+
 
   });
 }
