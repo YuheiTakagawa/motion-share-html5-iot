a4d8a5280816343fb2de313d8f644163e3f9a76d
yuheiTakagawa <b1014223@fun.ac.jp>
2016-10-14 03:31:28 +0900

    [fix]スケジュール選択時にバグが発生してもJSが止まらないように修正

--- motionshare/www/js/home.js
+++ motionshare/www/js/home.js
@@ -5,24 +5,24 @@ function homeInitilize(){
     var note="スケジュールはありません"
     if(!(localStorage.schedule===void 0)){
       var json=JSON.parse(localStorage.schedule);
-        var index='0';
+      var index='0';
       if(!(sessionStorage.scheduleIndex===void 0)){
         index=sessionStorage.scheduleIndex;
       }
-      if(Object.keys(json)!=""){
-        var date=json[index].date;
-        var dateMatch = date.match(/(\d+)-(\d+)-(\d+)T(\d+):(\d+)/);
-        //日付オブジェクトに変換
-        var dateObj = new Date(
-          +dateMatch[1],     //年
-          +dateMatch[2],     //月
-          +dateMatch[3],     //日
-          +dateMatch[4],     //時
-          +dateMatch[5],     //分
-          +0                 //秒
-        );
-        date=(dateMatch[2])+"/"+dateMatch[3]+" "+dateMatch[4]+":"+dateMatch[5];
-        note=json[index].note;
+      if((Object.keys(json)!="")&&!(json[index]===void 0)){
+          var date=json[index].date;
+          var dateMatch = date.match(/(\d+)-(\d+)-(\d+)T(\d+):(\d+)/);
+          //日付オブジェクトに変換
+          var dateObj = new Date(
+            +dateMatch[1],     //年
+            +dateMatch[2],     //月
+            +dateMatch[3],     //日
+            +dateMatch[4],     //時
+            +dateMatch[5],     //分
+            +0                 //秒
+          );
+          date=(dateMatch[2])+"/"+dateMatch[3]+" "+dateMatch[4]+":"+dateMatch[5];
+          note=json[index].note;
       }
     }
     $("#recentScheduleDate").html(date);
