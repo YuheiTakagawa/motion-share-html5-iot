56cac5c03a417c72729837331c7f2ae7740bcc42
yuheiTakagawa <b1014223@fun.ac.jp>
2016-10-14 18:01:03 +0900

    [add]ページ遷移しなくてもスケジュールが自動で削除できるように変更

--- motionshare/www/js/home.js
+++ motionshare/www/js/home.js
@@ -1,6 +1,22 @@
 function homeInitilize(){
-
   $(function(){
+
+    setInterval(function(){
+      if(autoScheduleDelete()=="1"){
+        //削除状態のJSONをローカルストレージに保存する
+        localStorage.schedule=JSON.stringify(scheduleJson);
+        sessionStorage.scheduleIndex='0';
+        scheduleFanc.readySchedule();
+        homeScheIni();
+      }
+    },60*1000);
+    homeScheIni();
+  });
+}
+
+function homeScheIni(){
+  $(function(){
+
     var date="--/-- --:--";
     var note="スケジュールはありません"
     if(!(localStorage.schedule===void 0)){
@@ -10,19 +26,19 @@ function homeInitilize(){
         index=sessionStorage.scheduleIndex;
       }
       if((Object.keys(json)!="")&&!(json[index]===void 0)){
-          var date=json[index].date;
-          var dateMatch = date.match(/(\d+)-(\d+)-(\d+)T(\d+):(\d+)/);
-          //日付オブジェクトに変換
-          var dateObj = new Date(
-            +dateMatch[1],     //年
-            +dateMatch[2],     //月
-            +dateMatch[3],     //日
-            +dateMatch[4],     //時
-            +dateMatch[5],     //分
-            +0                 //秒
-          );
-          date=(dateMatch[2])+"/"+dateMatch[3]+" "+dateMatch[4]+":"+dateMatch[5];
-          note=json[index].note;
+        var date=json[index].date;
+        var dateMatch = date.match(/(\d+)-(\d+)-(\d+)T(\d+):(\d+)/);
+        //日付オブジェクトに変換
+        var dateObj = new Date(
+          +dateMatch[1],     //年
+          +dateMatch[2],     //月
+          +dateMatch[3],     //日
+          +dateMatch[4],     //時
+          +dateMatch[5],     //分
+          +0                 //秒
+        );
+        date=(dateMatch[2])+"/"+dateMatch[3]+" "+dateMatch[4]+":"+dateMatch[5];
+        note=json[index].note;
       }
     }
     $("#recentScheduleDate").html(date);
