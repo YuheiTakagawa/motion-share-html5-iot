33bb95abdd79e8eb614f99112e9bf4089251b1c5
yuheiTakagawa <b1014223@fun.ac.jp>
2016-10-16 00:45:38 +0900

    [add]登録したユーザ情報をメニューに反映

--- motionshare/www/js/userSetting.js
+++ motionshare/www/js/userSetting.js
@@ -10,7 +10,7 @@ function setUserInfo(){
     };
     localStorage.contact=JSON.stringify(users);
     alert("ユーザ情報を保存しました．");
-    var index=1;
+    var index=2;
     var order = "nth-child("+index+")";
     $("nav ul li:"+order).css("background-color", "#00bcd4"); //選択された項目の背景色をcyanに変更
     $("nav ul li:not(:"+order+")").css("background-color", "#eceff1"); //選択外項目の色をサイドバー背景色にする
@@ -32,6 +32,17 @@ function showUserInfo(){
       $("#userMail").val(users["Mail"]);
       $("#userPass1").val(users["Pass1"]);
       $("#userPass2").val(users["Pass2"]);
+    }
+  });
+}
+
+function showUserMenu(){
+  $(function(){
+    if(!(localStorage.contact===void 0)){
+      var users=JSON.parse(localStorage.contact);
+      $("#userNameM").html(users["Name"]);
+      $("#userPhoneM").html(users["Phone"]);
+      $("#userMailM").html(users["Mail"]);
     }
   });
 }
