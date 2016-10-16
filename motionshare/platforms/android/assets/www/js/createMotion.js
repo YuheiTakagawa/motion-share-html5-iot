a648b2cb1c925a8462ca33091e6ace6833910b28
Koki Watanabe <gateway0616@gmail.com>
2016-10-16 18:24:49 +0900

    [add] indexで createMoton.js読み込み

---
+++ motionshare/platforms/android/assets/www/js/createMotion.js
@@ -0,0 +1,29 @@
+(function () {
+
+
+  $(function () {
+    window.addEventListener("devicemotion", devicemotionHandler);
+  });
+
+  // 加速度が変化
+  function devicemotionHandler(event) {
+
+    // 加速度
+    var x = event.acceleration.x;
+    var y = event.acceleration.y;
+    var z = event.acceleration.z;
+
+    //重力加速度
+    var xg = event.accelerationIncludingGravity.x;	// 横方向の傾斜
+    var yg = event.accelerationIncludingGravity.y;	// 縦方向の傾斜
+    var zg = event.accelerationIncludingGravity.z;	// 上下方向の傾斜
+
+    $('.x').html(x);
+    $('.y').html(y);
+    $('.z').html(z);
+
+    $('.xg').html(xg);
+    $('.yg').html(yg);
+    $('.zg').html(zg);
+
+  })();
