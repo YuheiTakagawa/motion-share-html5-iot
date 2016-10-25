52623bf6a03ab4c37d4e1d2e079b94405fffdca9
yuheiTakagawa <b1014223@fun.ac.jp>
2016-10-26 01:52:48 +0900

    [add]アカウント作成のサーバ連携実装（未検証・コメントアウト）

--- motionshare/www/js/userSetting.js
+++ motionshare/www/js/userSetting.js
@@ -8,17 +8,32 @@ function setUserInfo(){
       "Pass1":$("#userPass1").val(),
       "Pass2":$("#userPass2").val()
     };
+    /*
+    socket.emit('tekitou',users);
+    socket.on("tekito",function(e){
+    if(e==1){
+    */
+    $("#modeStatus").show();
+    $("#slide-menu").show();
+    $("#fab").show();
+    PageControll(0);
     localStorage.contact=JSON.stringify(users);
     alert("Saved user information");
-    var index=2;
-    var order = "nth-child("+index+")";
-    $("nav ul li:"+order).css("background-color", "#00bcd4"); //選択された項目の背景色をcyanに変更
-    $("nav ul li:not(:"+order+")").css("background-color", "#eceff1"); //選択外項目の色をサイドバー背景色にする
+    /*
+  }else{
+  alert("imika");
+}
+});
+*/
+var index=2;
+var order = "nth-child("+index+")";
+$("nav ul li:"+order).css("background-color", "#00bcd4"); //選択された項目の背景色をcyanに変更
+$("nav ul li:not(:"+order+")").css("background-color", "#eceff1"); //選択外項目の色をサイドバー背景色にする
 
-    $("nav ul li:"+order+" a").css("color", "#fff");//選択された項目のtxt色を白に変更
-    $("nav ul li:not(:"+order+") a").css("color", "#616161");//選択された項目のtxt色を黒に変更
-    PageControll(0);
-  });
+$("nav ul li:"+order+" a").css("color", "#fff");//選択された項目のtxt色を白に変更
+$("nav ul li:not(:"+order+") a").css("color", "#616161");//選択された項目のtxt色を黒に変更
+
+});
 }
 
 
