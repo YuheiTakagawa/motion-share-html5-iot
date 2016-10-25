fe832fc88240dbbbc72be7b7b230693906c8b356
Koki Watanabe <gateway0616@gmail.com>
2016-10-25 10:52:42 +0900

    [Delete] チームHTML5 のいままでの苦労の歴史を削除（XDKディレクトリと第一回合宿デモ用プログラムの削除）

--- _motionshere/platforms/android/assets/www/js/connectServer.js
+++
@@ -1,6 +0,0 @@
-//recodemo.herokuapp.com
-var url = "https://motion-share.herokuapp.com"; //websocketサーバのURL。
-var socket = io.connect(url);
-socket.on("connect", function() {
-  $(".info").text('connect server...OK');
-});
