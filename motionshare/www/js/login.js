a7bfa61f163a1ff1d68e9ec440c0287e02d326f8
Koki Watanabe <gateway0616@gmail.com>
2016-11-17 14:47:12 +0900

    [add] ログイン・アカウント作成機能実装完了

--- motionshare/www/js/login.js
+++ motionshare/www/js/login.js
@@ -1,10 +1,36 @@
 //ログイン処理
-function sendAccountInfo(){
-  var users = JSON.parse(localStorage.contact);
-  var id = users["Id"];
-  var pass = users["Pass1"];
+function setAccountInfo(){
+  $(function(){
+    var users={
+      "Name":$("#userName").val(),
+      "Id":$("#userId").val(),
+      "Phone":$("#userPhone").val(),
+      "Mail":$("#userMail").val(),
+      "Pass1":$("#userPass1").val(),
+      "Pass2":$("#userPass2").val()
+    };
+    localStorage.contact=JSON.stringify(users);
+  });
+}
 
-  socket.emit("check sign up", [ id , pass]);
+
+function sendAccountInfo(){
+  var id = $("#userId").val();
+  var pass = $("#userPass1").val();
+  alert(id + "," + pass);
+
+  socket.emit("check sign up", id);
+
+  socket.on("verify sign up", function(data){
+    if(data == 1){
+      alert("success");
+      $("#view").load("login.html",function(){
+        socket.emit("finalize sign up", [ id , pass ]);
+      });
+    }else{
+      alert("failed");
+    }
+  });
 }
 
 
@@ -13,10 +39,11 @@ function requestLogin(){
   var pass = $("#loginPass").val();
   //alert(id + ':' + pass);
 
-  socket.emit("request log in", [ id , pass]);
+  socket.emit("request log in", [ id , pass ]);
 
-  socket.on('verify log in', function(id){
-    if(id == 1){
+  socket.on('verify log in', function(data){
+    if(data == 4649){
+      alert("success");
       PageControll(0);
     }else{
       alert("failed");
