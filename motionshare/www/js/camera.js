1d887dcfa69196ee1c791d2fdd76752b6f162cf0
yuheiTakagawa <b1014223@fun.ac.jp>
2016-11-27 11:27:12 +0900

    [add]一度でリサイズを上限値まで可能に変更

--- motionshare/www/js/camera.js
+++ motionshare/www/js/camera.js
@@ -70,19 +70,23 @@ function getCameraBase64(){
 
       // 縮小する。今回は縦横それぞれ1/2
       var canvas = document.createElement("canvas");
-      var n = 2;//ここは10
-      canvas.width = width / n;
-      canvas.height = height / n;
-      if(canvas.width*canvas.height>921600){
-      canvas.getContext("2d").drawImage(image, 0, 0, width / n, height / n);
+      var n = 3;//ここは10
+//ばぐ
+      while(width > 1000 && height >1000){
+        width=2*width/n;
+        height=2*height/n;
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext("2d").drawImage(image, 0, 0, width, height);
 
-      var data=canvas.toDataURL("image/jpeg",1.0);
-      // データURLにして返す。他にバイナリを返す toBlob() メソッドもあります。
-      data=data.replace(/data:image[\/]jpeg;base64,/g,"");
-      //alert(data);
-      $("#camera_pic").attr("src",'data:image/jpeg;charset=utf-8;base64,'+data);
-      localStorage.setItem('imageData',data);
-    }
+        var data=canvas.toDataURL("image/jpeg",1.0);
+        // データURLにして返す。他にバイナリを返す toBlob() メソッドもあります。
+        data=data.replace(/data:image[\/]jpeg;base64,/g,"");
+        //alert(data);
+        $("#camera_pic").attr("src",'data:image/jpeg;charset=utf-8;base64,'+data);
+        localStorage.setItem('imageData',data);
+        }
+
       // JPEG形式のほうが良い圧縮率が得られると思われます。
       // 第2引数は品質レベルで、0.0~1.0の間の数値です。高いほど高品質。
       // return canvas.toDataURL("image/jpeg", 0.5);
