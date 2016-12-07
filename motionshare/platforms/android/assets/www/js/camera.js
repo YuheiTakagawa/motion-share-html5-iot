7b9cb8c1c80316368c81d795a818a8928605f13c
Koki Watanabe <gateway0616@gmail.com>
2016-12-07 18:09:24 +0900

    [modify] カメラのボタン サイズ変更

--- motionshare/platforms/android/assets/www/js/camera.js
+++ motionshare/platforms/android/assets/www/js/camera.js
@@ -6,6 +6,7 @@ function getCameraBase64(){
       //localStorage.setItem('imageData', imageData);
       $('#camera_pic').attr('src', 'data:image/jpeg;charset=utf-8;base64,' + imageData);
       makeSmall();
+      senderMode();
     },
     function(message){
       // cameraError
@@ -29,6 +30,7 @@ function getCameraBase64(){
         //localStorage.setItem('imageData', imageData);
         $('#camera_pic').attr('src', 'data:image/jpeg;charset=utf-8;base64,' + imageData);
         makeSmall();
+        senderMode();
       },
       function(message){
         // cameraError
@@ -67,12 +69,13 @@ function getCameraBase64(){
       var width = $("#camera_pic").get(0).naturalWidth;
       var height = $("#camera_pic").get(0).naturalHeight;
 
+
       //Materialize.toast(width+","+height, 2000);
 
       // 縮小する。今回は縦横それぞれ1/2
       var canvas = document.createElement("canvas");
       var n = 3;//ここは10
-      while(width > 1000 && height >1000){
+      while(width > 1000 || height >1000){
         width=2*width/n;
         height=2*height/n;
         canvas.width = width;
@@ -85,7 +88,8 @@ function getCameraBase64(){
         //alert(data);
         $("#camera_pic").attr("src",'data:image/jpeg;charset=utf-8;base64,'+data);
         localStorage.setItem('imageData',data);
-        }
+      }
+      //alert(width+","+height);
       // JPEG形式のほうが良い圧縮率が得られると思われます。
       // 第2引数は品質レベルで、0.0~1.0の間の数値です。高いほど高品質。
       // return canvas.toDataURL("image/jpeg", 0.5);
